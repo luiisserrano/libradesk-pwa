2b7e1af916957246f014040aa15cd410faeb4995
JulieValdes <julieworks2@hotmail.com>
2025-12-08 07:25:15 -0600

    ya llega correo

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-435d9fc8'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.omq1e598hqo"
+    "revision": "0.atf80spq2ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
